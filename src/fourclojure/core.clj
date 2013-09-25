b044ed5c6adde3c2fee482e6e65bcef8f86d3065
paulbowler <paul_bowler@yahoo.com>
2013-09-25 11:22:09 +0100

    Added solutions into core

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -1,6 +1,40 @@
+;;
+;; 4clojure.com Solutions, 2012 Paul Bowler
+;; Distributed under the Eclipse Public License, the same as 4Clojure.
+;;
+
 (ns fourclojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; 1. Simple Maths
+
+(= true true)
+
+(= (not false) true)
+
+; 2. Simple Maths
+
+(= (- 10 (* 2 3)) 4)
+
+; 3. Intro to Strings
+
+(= "HELLO WORLD" (.toUpperCase "hello world"))
+
+; More idiomatic to use clojure upper-string function rather than java interop
+
+(= "HELLO WORLD" (clojure.string/upper-case "hello world"))
+
+; Resulting data are identical
+
+(= (clojure.string/upper-case "hello world") (.toUpperCase "hello world"))
+
+; Not surprising as upper-case uses Java under the hood, and Java strings are immutable:
+
+(defn ^String upper-case
+  "Converts string to all upper-case."
+  {:added "1.2"}
+  [^CharSequence s]
+  (.. s toString toUpperCase))
+
+; 4. Intro to Lists
+
+(= (list :a :b :c) '(:a :b :c))
