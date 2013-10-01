3d0a09d6bcdf2ac310ca8cbbe68e98d4a10e551a
paulbowler <paul_bowler@yahoo.com>
2013-10-01 13:20:11 +0100

    Added answers up to Q69

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -643,6 +643,29 @@
 (= (prime-seq 5) [2 3 5 7 11])
 (= (last (prime-seq 100)) 541)
 
+; 68. Recurring Theme
+
+(= [7 6 5 4 3]
+  (loop [x 5
+         result []]
+    (if (> x 0)
+      (recur (dec x) (conj result (+ 2 x)))
+      result)))
+
+; 69. Merge with a Function - without using merge-with
+
+(defn my-merge-with [f & m]
+  (let [unique-keys (distinct (flatten (map keys m)))
+        values (map (fn [k] (reduce f (filter identity (map #(get % k) m)))) unique-keys)]
+    (zipmap unique-keys values)))
+
+(= (my-merge-with * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
+   {:a 4, :b 6, :c 20})
+(= (my-merge-with - {1 10, 2 20} {1 3, 2 10, 3 15})
+   {1 7, 2 10, 3 15})
+(= (my-merge-with concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
+   {:a [3 4 5], :b [6 7], :c [8 9]})
+
 
 ; 150. Palindromic Numbers
 
