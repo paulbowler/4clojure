144b5ee007916b599670738c8cec940c7cc96db0
paulbowler <paul_bowler@yahoo.com>
2013-10-15 10:50:30 +0100

    Added corrected solution to Q147

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -1232,7 +1232,7 @@
 (= (second (pascal [2 3 2])) [2 5 5 2])
 (= (take 5 (pascal [1])) [[1] [1 1] [1 2 1] [1 3 3 1] [1 4 6 4 1]])
 (= (take 2 (pascal [3 1 2])) [[3 1 2] [3 4 3 2]])
-(= (take 100 (pascal [2 4 2])) (rest (take 101 (__ [2 2]))))
+(= (take 100 (pascal [2 4 2])) (rest (take 101 (pascal [2 2]))))
 
 ; 150. Palindromic Numbers
 
